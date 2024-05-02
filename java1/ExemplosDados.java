01886eae40e8d8c70886aacad08dcc8732c1fdc0
lucas <blesslucas89@gmail.com>
2024-05-02 18:19:44 -0400

    booleano

---
+++ java1/ExemplosDados.java
@@ -0,0 +1,13 @@
+//declaracao de variaveis
+//int idade;
+//double preco;
+
+//int anoNacimento = 1985;
+
+public class ExemplosDados {
+    public static void main(String[] args) {
+        int idade = 10;
+        System.out.println(idade);
+    }
+}
+
---
+++ java1/exemplobolean.java
@@ -0,0 +1,9 @@
+public class exemplobolean {
+    
+    public static void main(String[] args) {
+        boolean Fumante  = false;
+        System.out.println(Fumante);
+    }
+}
+
+    
